36b6e8294a52589e284c6e2cbad2951b9f6a90a2
Amar Alibegovic <amaralibegovic89@gmail.com>
2021-09-27 17:19:26 +0200

    first commit

---
+++ ex08/src/index.js
@@ -0,0 +1,8 @@
+// only change code below this line
+const sum = (num1, num2 = 1) => {
+    return num1 + num2;
+}
+// only change code above this line
+
+console.log(sum(2, 8));
+module.exports = sum;
